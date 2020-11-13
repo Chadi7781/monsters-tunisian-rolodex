45108cbe66d7b3e9d8703d4cba23fcbfa06a95a3
Chedi <chadi.troudi@esprit.tn>
2020-11-13 16:36:45 +0100

    Updates

---
+++ precache-manifest.89fa0581fc14a370aa22217d0fad79ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8577e304dc40172c1be",
+    "url": "/monsters-tunisian-rolodex/static/js/runtime~main.c9c86637.js"
+  },
+  {
+    "revision": "8eb1d70b4aaa86331b7b",
+    "url": "/monsters-tunisian-rolodex/static/js/main.8da09d56.chunk.js"
+  },
+  {
+    "revision": "a197c4cbda3883e11c3e",
+    "url": "/monsters-tunisian-rolodex/static/js/2.5e199a50.chunk.js"
+  },
+  {
+    "revision": "8eb1d70b4aaa86331b7b",
+    "url": "/monsters-tunisian-rolodex/static/css/main.6fbc8967.chunk.css"
+  },
+  {
+    "revision": "f06a5291cdf5d022abd19cb7b82e129f",
+    "url": "/monsters-tunisian-rolodex/index.html"
+  }
+];
